13b99d3a068ace6c93382a8a5ba7898f23028b1a
StraetenMax <maxstraeten@yahoo.com>
2017-06-10 17:27:46 +0200

    class Facture méthodes

--- src/examCnam/Facture.java
+++ src/examCnam/Facture.java
@@ -17,4 +17,35 @@ public class Facture extends DocImprime{
 		this.setTypeFact();
 	}
 	
-}
+	//accesseurs et mutateurs
+	public int getMontant(){
+		return montant;
+	}
+	public String getTypeFact(){
+		return typeFact;
+	}
+	public void setMontant(int montant){
+		this.montant=montant;
+		this.setTypeFact();
+	}
+	public void setTypeFact(){
+		if(this.montant<0){
+			typeFact="Facture d'avoir";
+		}else{
+			typeFact="Facture";
+		}
+	}
+	//méthodes
+	public boolean EstSuperieurA(Facture f1){
+		boolean str;
+		if(this.montant > f1.getMontant()){
+			str = true;
+		}else{
+			str = false;
+		}return str;
+	}
+	public String description(){
+		String str1 = super.description()+" dont le montant est de"+this.getMontant()+" euros";
+		return str1;
+	}
+	}
