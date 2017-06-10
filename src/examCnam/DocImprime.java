260bb4ff8e75c3846d279f24fd606de1d5ff32bf
StraetenMax <maxstraeten@yahoo.com>
2017-06-10 16:32:07 +0200

    correction

--- src/examCnam/DocImprime.java
+++ src/examCnam/DocImprime.java
@@ -1,5 +1,17 @@
 package examCnam;
 
-public class DocImprime {
+public class DocImprime extends Document {
+	//propriétés
+	private String dateImprim;
+	
+	//constructeurs vide et initialisé
+	public DocImprime(){
+		super();
+		dateImprim = "";
+	}
+	public DocImprime(String codeDocument, int nbPages,String dateImprim){
+		super(codeDocument,nbPages);
+		this.dateImprim = dateImprim;
+	}
 
 }
