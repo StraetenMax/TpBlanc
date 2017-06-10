cc4e176d376f756582f182b3b14e936b091a8069
StraetenMax <maxstraeten@yahoo.com>
2017-06-10 17:57:28 +0200

    class codeDocumentException

---
+++ src/examCnam/codeDocumentException.java
@@ -0,0 +1,9 @@
+package examCnam;
+
+public class codeDocumentException extends Exception{
+	
+	public codeDocumentException(){
+		System.out.println("Instanciation de document avec code vide");
+	}
+
+}
