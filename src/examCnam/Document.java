c7af5b359e176590ffdf1b5fb3b914e9d95d7383
StraetenMax <maxstraeten@yahoo.com>
2017-06-10 16:11:32 +0200

    class Document

---
+++ src/examCnam/Document.java
@@ -0,0 +1,39 @@
+package examCnam;
+
+public class Document {
+	//propriétés
+	private String codeDocument;
+	private int nbPages;
+	
+	//constructeurs vide et initialisé
+	public Document(){
+		codeDocument ="";
+		nbPages = 0;
+	}
+	
+	public Document(String codeDocument, int nbPages){
+		this.codeDocument = codeDocument;
+		this.nbPages = nbPages;
+	}
+	
+	//mutateurs et accesseurs
+	public String getCodeDocument(){
+		return codeDocument;
+	}
+	public int getNbPages(){
+		return nbPages;
+	}
+	public void setCodeDocument(String codeDocument){
+		this.codeDocument = codeDocument;
+	}
+	public void setNbPages(int nbPages){
+		this.nbPages = nbPages;
+	}
+	
+	//méthodes
+	public String 
+	
+	
+}
+	
+
